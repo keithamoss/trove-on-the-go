3d53269c37c9f0614bfead01225648e79debc968
keithamoss <keithamoss@gmail.com>
2020-03-21 17:39:31 +0800

    Updates

---
+++ precache-manifest.bfacbd68f9e7dcc9d89e4d9013f78821.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba913a22f075060e87d2ceaf37cd5aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be9e1f0d0ca6449ab4d",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "25162069368c399ecb18",
+    "url": "/static/js/2.57f2a42f.chunk.js"
+  },
+  {
+    "revision": "e70bb637694db0b4f7783ec9c2471334",
+    "url": "/static/js/2.57f2a42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be9e1f0d0ca6449ab4d",
+    "url": "/static/js/main.8e21363c.chunk.js"
+  },
+  {
+    "revision": "52997f93f6eec3bff30d",
+    "url": "/static/js/runtime-main.4c83eae0.js"
+  },
+  {
+    "revision": "a96d368a8c56c89785602902d5f6811d",
+    "url": "/static/media/undraw_cup_of_tea.a96d368a.svg"
+  }
+]);
