8c2e9a37206df7856a572ecef61fdc9db76de3d3
Keith Moss <keithamoss@gmail.com>
2020-04-09 07:21:43 +0800

    Remove Zappa WIP
    It doesn't play nicely with FastAPI/Flask and Asyncio and whatnot yet. PITA, so switched to NodeJS/TypeScript.

--- zappa/__main__.py
+++
@@ -1,6 +0,0 @@
-import asgi_app.app
-
-
-def main():
-    app = asgi_app.app.create_app()
-    app.run()
--- zappa/dev.sh
+++
@@ -1,5 +0,0 @@
-# export FLASK_ENV=development
-# export FLASK_APP=trove.py
-# flask run
-
-uvicorn trove:app --reload
