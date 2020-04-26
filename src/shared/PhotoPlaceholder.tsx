3b14a011e5d47d7013304bf33bfb58b5ba2015d1
Keith Moss <keithamoss@gmail.com>
2020-04-26 09:15:52 +0800

    Placeholder loading SVG is now properly responsive

--- src/shared/PhotoPlaceholder.tsx
+++ src/shared/PhotoPlaceholder.tsx
@@ -3,21 +3,25 @@ import ContentLoader from 'react-content-loader'
 
 export const PhotoPlaceholder = (props: any) => (
   <ContentLoader
-    viewBox="0 0 820 450"
-    height={350}
-    width={820}
-    backgroundColor="#f5f6f7"
+    viewBox="0 0 550 370"
+    // backgroundColor="#f5f6f7"
     foregroundColor="#e4e4e4"
-    style={{ width: '100%' }}
+    style={{opacity: 0.8, marginBottom: 15}}
     {...props}
   >
-    <rect x="10" y="0" rx="5" ry="5" width="640" height="40" />
-    <rect x="10" y="50" rx="5" ry="5" width="320" height="30" />
-    <rect x="10" y="90" rx="5" ry="5" width="260" height="140" />
-    <rect x="280" y="90" rx="5" ry="5" width="260" height="280" />
-    <rect x="550" y="90" rx="5" ry="5" width="260" height="140" />
-    <rect x="10" y="240" rx="5" ry="5" width="260" height="280" />
-    <rect x="280" y="380" rx="5" ry="5" width="260" height="140" />
-    <rect x="550" y="240" rx="5" ry="5" width="260" height="280" />
+    {/* Title */}
+    <rect x="10" y="10" rx="5" ry="5" width="430" height="25" />
+    {/* Subtitle */}
+    <rect x="10" y="40" rx="5" ry="5" width="215" height="20" />
+
+    {/* First Row */}
+    <rect x="10" y="70" rx="5" ry="5" width="170" height="95" />
+    <rect x="185" y="70" rx="5" ry="5" width="170" height="165" />
+    <rect x="360" y="70" rx="5" ry="5" width="170" height="95" />
+
+    {/* Second Row */}
+    <rect x="10" y="170" rx="5" ry="5" width="170" height="190" />
+    <rect x="185" y="240" rx="5" ry="5" width="170" height="120" />
+    <rect x="360" y="170" rx="5" ry="5" width="170" height="190" />
   </ContentLoader>
 )
