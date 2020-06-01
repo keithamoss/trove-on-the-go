8b285dd74b924ca01afaa731b075d895d0a2f60b
Keith Moss <keithamoss@gmail.com>
2020-06-01 12:31:51 +0800

    Testing husky

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -120,7 +120,7 @@ const Home: React.FC<RouteComponentProps> = ({ history }: RouteComponentProps) =
             <Grid container style={{ marginTop: 25 }}>
               <Grid item xs>
                 <Link href="https://trove.nla.gov.au/" variant="overline" display="block" align="center">
-                  Made with Trove{' '}
+                  Made with Trovez{' '}
                   <span role="img" aria-label="Love heart emoji">
                     ❤️
                   </span>
