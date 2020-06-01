6e0aa38ece140cdfeb77fda381cb59da323e7843
Keith Moss <keithamoss@gmail.com>
2020-06-01 12:57:48 +0800

    Ignore eslint complaining about ts-ignore-next-line (for now)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 
 const App: React.FC = () => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore-next-line
     defaultMatches: null,
   })
