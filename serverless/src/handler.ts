80d8b9b45f0676929023425c540106c22d225342
Keith Moss <keithamoss@gmail.com>
2020-12-24 06:39:00 +0800

    Don't print results from dev

--- serverless/src/handler.ts
+++ serverless/src/handler.ts
@@ -2,7 +2,6 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
 import TroveResult from './handlers/trove_result'
 import { LambdaApiError, makeResponse } from './lib/response'
-import { isDev } from './lib/utils'
 import { TroveApiResponse } from './types'
 
 // : Handler
@@ -12,10 +11,10 @@ exports.trove_result = (
   // callback: Callback
 ) => {
   TroveResult(event, (error: LambdaApiError | null, result: TroveApiResponse | Record<string, string>) => {
-    if (isDev() === true) {
-      // eslint-disable-next-line
-      console.log(JSON.stringify(result, undefined, 4))
-    }
+    // if (isDev() === true) {
+    //   // eslint-disable-next-line
+    //   console.log(JSON.stringify(result, undefined, 4))
+    // }
     context.succeed(makeResponse(error, result))
   })
 }
