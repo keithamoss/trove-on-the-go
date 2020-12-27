07044f7825cd751512e9e2f888caaf488a0a6442
Keith Moss <keithamoss@gmail.com>
2020-12-27 10:58:24 +0800

    Revert simple-react-lightbox and friends experiments

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,9 +2,3 @@
 /// <reference types="react-scripts" />
 
 declare module 'react-grid-gallery'
-declare module 'simple-react-lightbox'
-
-interface Navigator extends Navigator {
-  share?: (options: any) => Promise<void>
-  canShare?: (options: any) => boolean
-}
