9e93ff134d9af9283ad9c1c94b4896eb243f57cc
Keith Moss <keithamoss@gmail.com>
2021-01-26 11:31:34 +0800

    Slightly cleaned up TimelineScrubber sans RTL support

--- src/components/photo-listing/TimelineScrubber.tsx
+++ src/components/photo-listing/TimelineScrubber.tsx
@@ -1,4 +1,4 @@
-import { Box, makeStyles, Slider, Theme, ThemeProvider, Typography, withStyles, withTheme } from '@material-ui/core'
+import { Box, makeStyles, Slider, Theme, withStyles } from '@material-ui/core'
 import ValueLabel from '@material-ui/core/Slider/ValueLabel'
 import React, { Fragment } from 'react'
 import { useLocation } from 'react-router-dom'
@@ -6,24 +6,11 @@ import styled from 'styled-components'
 import { TroveAPIWorkDateCount } from '../../api/types'
 import useTroveAPI from './useTroveDateAPIHook'
 
-// export interface GalleryPhotos {
-//   photoIndex: number
-//   photos: TrovePhotoMetadata[]
-// }
-
 type TimelineScrubberProps = {
   searchTerm: string
-  // searchYear: number | null
-  onDateChange: any
-  theme: Theme
+  onDateChange: (year: number) => void
 }
 
-// const themeRTL = createMuiTheme({
-//   direction: 'rtl',
-// })
-// console.log('themeRTL', themeRTL)
-
-// eslint-disable-next-line @typescript-eslint/no-shadow
 const useStyles = makeStyles((theme: Theme) => ({
   stickToBottom: {
     width: '100%',
@@ -36,98 +23,19 @@ const useStyles = makeStyles((theme: Theme) => ({
     top: 0,
     left: 0,
     justifyContent: 'center',
-    // backgroundColor: '#424242',
     backgroundColor: `${theme.palette.background.default}`,
   },
   labelLeft: {
     position: 'absolute',
     bottom: 5,
     left: 10,
-    // color: 'rgba(255, 255, 255, 0.7)',
-    // fontSize: '12px',
   },
   labelRight: {
     position: 'absolute',
     bottom: 5,
     right: 10,
-    // color: 'rgba(255, 255, 255, 0.7)',
-    // fontSize: '12px',
   },
 }))
-
-// const iOSBoxShadow = '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)'
-
-// const IOSSlider = withStyles({
-//   // root: (props: any) => ({
-//   //   '& .MuiSlider-markLabel': {
-//   //     display: 'none',
-//   //     // display: props.min,
-//   //   },
-//   //   '& .MuiSlider-markLabel[data-index="0"]': {
-//   //     display: 'block',
-//   //     transform: 'translateX(0%)',
-//   //   },
-//   //   '& .MuiSlider-markLabel[data-index="$props.min"]': {
-//   //     display: 'block',
-//   //     transform: 'translateX(-100%)',
-//   //   },
-//   // }),
-//   // root: {
-//   //   color: '#3880ff',
-//   //   height: 2,
-//   //   padding: '15px 0',
-//   // },
-//   // thumb: {
-//   //   height: 28,
-//   //   width: 28,
-//   //   backgroundColor: "#fff",
-//   //   boxShadow: iOSBoxShadow,
-//   //   marginTop: -14,
-//   //   marginLeft: -14,
-//   //   "&:focus, &:hover, &$active": {
-//   //     boxShadow:
-//   //       "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
-//   //     // Reset on touch devices, it doesn't add specificity
-//   //     "@media (hover: none)": {
-//   //       boxShadow: iOSBoxShadow
-//   //     }
-//   //   }
-//   // }
-//   // active: {}
-//   // valueLabel: {
-//   //   left: 'calc(-50% + 12px)',
-//   //   top: -22,
-//   //   '& *': {
-//   //     background: 'transparent',
-//   //     color: '#000',
-//   //   },
-//   // },
-//   // track: {
-//   //   height: 2,
-//   // },
-//   // rail: {
-//   //   height: 2,
-//   //   opacity: 0.5,
-//   //   backgroundColor: '#bfbfbf',
-//   // },
-//   // mark: {
-//   //   backgroundColor: '#bfbfbf',
-//   //   height: 8,
-//   //   width: 1,
-//   //   marginTop: -3,
-//   // },
-//   mark: {
-//     // backgroundColor: "#bfbfbf",
-//     height: 8,
-//     width: 1,
-//     marginTop: -3,
-//     marginLeft: -1,
-//   },
-//   markActive: {
-//     opacity: 1,
-//     backgroundColor: 'currentColor',
-//   },
-// })(Slider)
 
 /**
  * Normalizes a value from one range (current) to another (new).
@@ -144,9 +52,10 @@ const normalizeBetweenTwoRanges = (val: number, minVal: number, maxVal: number, 
   return newMin + ((val - minVal) * (newMax - newMin)) / (maxVal - minVal)
 }
 
-const getDefaultSearchYear = (query: URLSearchParams): number | null => {
-  if (query.get('y') !== null) {
-    return Number.parseInt(query.get('y')!, 10)
+const getSearchYearFromQS = (query: URLSearchParams): number | null => {
+  const y = query.get('y')
+  if (y !== null) {
+    return Number.parseInt(y, 10)
   }
 
   return null
@@ -158,13 +67,14 @@ function useQuery() {
   return new URLSearchParams(useLocation().search)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const generateCSS = (props: any) => {
-  const counts: number[] = props.worksPerYear.map((item: any) => item.count)
+  const { worksPerYear }: { worksPerYear: TroveAPIWorkDateCount[] } = props
+  const counts: number[] = worksPerYear.map((item) => item.count)
 
-  // color: rgba(144, 202, 249, ${(item.count - Math.min(...counts)) / (Math.max(...counts) - Math.min(...counts))});
-  const css = props.worksPerYear
+  const css = worksPerYear
     .map(
-      (item: any, index: number) => `& span[class^=MuiSlider-mark-][data-index='${index}'] {
+      (item, index) => `& .MuiSlider-mark[data-index='${index}'] {
         color: rgba(144, 202, 249, ${normalizeBetweenTwoRanges(
           item.count,
           Math.min(...counts),
@@ -175,163 +85,88 @@ const generateCSS = (props: any) => {
       }`
     )
     .join('\n')
-  // console.log(css)
+  // console.log('!!!', css)
   return css
 }
 
-const IOSSlider1Point5 = (props: any) => {
+// Thanks to Material-UI issues with typings for extended componnents, we can't have nicely typed props here
+// https://github.com/mui-org/material-ui/issues/20191
+// https://github.com/mui-org/material-ui/issues/17454
+// @TODO According to the comments this should work fine in v5 (i.e. Using SliderProps and extending it)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const ExtendedSlider = (props: any) => {
   const { worksPerYear, ...other } = props
   return <Slider {...other} />
 }
 
-const IOSSlider2 = styled(IOSSlider1Point5)`
-  & span[class^='MuiSlider-mark-'] {
+const SliderSyled = styled(ExtendedSlider)`
+  & .MuiSlider-mark {
     height: 8px;
     width: 1px;
     margin-top: -3px;
     margin-left: -1px;
   }
-  & span[class^='MuiSlider-markActive-'] {
+  & .MuiSlider-markActive {
     opacity: 1;
     background-color: 'currentColor';
   }
-  & span[class^='MuiSlider-markLabel-'] {
+  & .MuiSlider-markLabel {
     display: none;
   }
-  /* & span[class^='MuiSlider-markLabel-'][data-index='0'] {
+  & .MuiSlider-markLabel[data-index='0'] {
     display: block;
   }
-  & span[class^='MuiSlider-markLabel-'][data-index='${(props: any) => props.marks.length - 1}'] {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  & .MuiSlider-markLabel[data-index='${(props: any) => props.marks.length - 1}'] {
     display: block;
-    right: 0% !important;
-  } */
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ${(props: any) => generateCSS(props)}
 `
-
-const IOSSlider3 = (props: any) => {
-  return <IOSSlider2 {...props} />
-}
-
-function valuetext(value: any) {
-  return `${value}°C`
-}
-
-function valueLabelFormat(value: any) {
-  // console.log('valueLabelFormat', value)
-  // return `${value}°C`
-  if (isNaN(value)) {
-    return undefined
-  }
-  return value
-  // return marks.findIndex((mark) => mark.value === value) + 1;
-}
 
 // https://github.com/mui-org/material-ui/issues/16547
 const StyledValueLabel = withStyles((theme: Theme) => ({
   offset: {
     top: '-17px',
     transform: 'rotate(-180deg) scale(1) translateY(-10px) !important',
-    // left: (props: any) => (props.index === 0 ? 'calc(-50% + -20px)' : 'calc(-50% + 12px)'),
   },
   thumb: {
-    // backgroundColor: '#90caf9',
     backgroundColor: `${theme.palette.primary.light}`,
-    // transform: (props: any) => (props.index === 0 ? 'rotate(-90deg)' : 'rotate(0deg)'),
   },
   circle: {
-    // backgroundColor: '#90caf9',
     backgroundColor: `${theme.palette.primary.light}`,
-    // transform: (props: any) => (props.index === 0 ? 'rotate(-90deg)' : 'rotate(0deg)'),
   },
   label: {
     transform: 'rotate(-135deg)',
-    // transform: (props: any) => (props.index === 0 ? 'rotate(90deg)' : 'rotate(0deg)'),
   },
 }))(ValueLabel)
 
-const TimelineScrubber: React.FC<TimelineScrubberProps> = ({
-  searchTerm,
-  // searchYear,
-  onDateChange,
-  theme,
-}: TimelineScrubberProps) => {
-  // const TimelineScrubber: React.FC<any> = () => {
+const TimelineScrubber: React.FC<TimelineScrubberProps> = ({ searchTerm, onDateChange }: TimelineScrubberProps) => {
   const classes = useStyles()
 
-  console.log('! searchTerm', searchTerm)
-
-  // console.log('TimelineScrubber.searchYear', searchYear)
-
-  // const marks = [
-  //   {
-  //     value: 0,
-  //     // label: '0°C',
-  //   },
-  //   {
-  //     value: 20,
-  //     // label: '20°C',
-  //   },
-  //   {
-  //     value: 21,
-  //   },
-  //   {
-  //     value: 22,
-  //   },
-  //   {
-  //     value: 23,
-  //   },
-  //   {
-  //     value: 24,
-  //   },
-  //   {
-  //     value: 34,
-  //   },
-  //   {
-  //     value: 37,
-  //     // label: '37°C',
-  //   },
-  //   {
-  //     value: 100,
-  //     // label: '100°C',
-  //   },
-  // ]
-
-  // console.log('searchTerm', searchTerm)
   const {
-    // state: { isLoading, isError, hasMoreResults, response },
-    // state: { isLoading, isError, response },
-    state: { isLoading, response },
-    // getNextPage,
+    state: { response },
   } = useTroveAPI(searchTerm)
-  console.log('response', response)
-  console.log('isLoading', isLoading)
 
   const query = useQuery()
-  console.log('Year in URL', query.get('y'))
 
-  const [searchYearLocal, setSearchYearLocal] = React.useState<number | null>(getDefaultSearchYear(query))
-  console.log('searchYearLocal', searchYearLocal)
+  const [searchYearLocal, setSearchYearLocal] = React.useState<number | null>(getSearchYearFromQS(query))
 
-  const [searchYearURL, setSearchYearURL] = React.useState<number | null>(getDefaultSearchYear(query))
+  const [searchYearURL, setSearchYearURL] = React.useState<number | null>(getSearchYearFromQS(query))
 
   const [sliderIsChanging, setSliderIsChanging] = React.useState<boolean>(false)
-  console.log('sliderIsChanging', sliderIsChanging)
 
   if (response === null) {
-    console.log('( Bail and do not bother trying to render')
+    // console.log('( Bail and do not bother trying to render')
     return null
   }
-
-  // console.log('isLoading', isLoading)
-  // console.log('isError', isError)
-  // console.log('response', response)
 
   const getMarks = () => {
     if (response !== null && worksPerYear !== null) {
       return worksPerYear.map((item: TroveAPIWorkDateCount) => {
         return {
           value: item.year,
-          // label: item.year,
+          label: item.year,
         }
       })
     }
@@ -340,124 +175,56 @@ const TimelineScrubber: React.FC<TimelineScrubberProps> = ({
 
   let worksPerYear: TroveAPIWorkDateCount[] | null = null
   if (response !== null) {
-    // console.log(response.worksPerYear[0])
-    worksPerYear = response.worksPerYear.reverse()
-    // console.log(worksPerYear[0])
-
-    // console.log(getMarks())
-    // console.log(marks)
+    worksPerYear = response.worksPerYear
   }
-
-  // & .MuiSlider-markLabel[data-index='0'] {
-  //   display: block;
-  // }
-
-  // let IOSSlider3 = <Fragment />
-  // if (response !== null) {
-  //   const counts: number[] = worksPerYear.map((item: any) => item.count)
-
-  //   const css = worksPerYear.map(
-  //     (item: any, index: number) => `& .MuiSlider-markLabel[data-index='${index}'] {
-  //       display: block;
-  //       color: rgba(144, 202, 249, ${
-  //         (item.count - Math.min(...counts)) / (Math.max(...counts) - Math.min(...counts))
-  //       }) !important;
-  //     }`
-  //   )
-  //   console.log(css.join('\n'))
-  //   console.log('counts', counts)
-  //   console.log('min count', Math.min(...counts))
-  //   console.log('max count', Math.max(...counts))
-
-  //   IOSSlider3 = styled(IOSSlider2)`
-  //     ${css}
-  //   `
-  //   console.log('IOSSlider3', IOSSlider3)
-  // }
 
   // @TODO Look at using this technique to scale years in a non-linear fashion:
   // https://stackoverflow.com/questions/61792449/material-ui-slider-changing-values-using-scale
 
-  // console.log('theme', theme)
-  // https://github.com/mui-org/material-ui/issues/18690
-  const newTheme = { ...theme, direction: 'rtl' }
-  // console.log('newTheme', newTheme)
-
-  if (query.get('y') != searchYearURL) {
-    console.log('Setting searchYearURL', query.get('y') === null ? null : Number.parseInt(query.get('y')!, 10))
-    setSearchYearURL(query.get('y') === null ? null : Number.parseInt(query.get('y')!, 10))
+  if (getSearchYearFromQS(query) !== searchYearURL) {
+    // console.log('Setting searchYearURL', getSearchYearFromQS(query))
+    setSearchYearURL(getSearchYearFromQS(query))
   }
 
   // This happens when the URL year has a value and it changes in resonse to onChangeCommitted OR when history() changes it
-  if (sliderIsChanging === false && searchYearURL != searchYearLocal && searchYearURL !== null) {
-    // if (sliderIsChanging === false && searchYearURL != searchYearLocal) {
-    console.log('> Setting searchYearLocal from searchYearURL', searchYearURL)
+  if (sliderIsChanging === false && searchYearURL !== searchYearLocal && searchYearURL !== null) {
+    // console.log('> Setting searchYearLocal from searchYearURL', searchYearURL)
     setSearchYearLocal(searchYearURL)
   }
 
-  // # This is what lets us load the year from the URL when navigate history() changes it
-  // if (response !== null && query.get('y') !== null && query.get('y') != searchYearLocal) {
-  // if (response !== null && query.get('y') != searchYearLocal) {
-  //   if (query.get('y')! === null && searchYearLocal === null) {
-  //     console.log('# Set from URL to null')
-  //     setSearchYearLocal(null)
-  //     setSearchYearURL(null)
-  //   } else if (searchYearURL != Number.parseInt(query.get('y')!, 10) && searchYearLocal !== null) {
-  //     console.log('# Set from URL', Number.parseInt(query.get('y')!, 10))
-  //     setSearchYearLocal(Number.parseInt(query.get('y')!, 10))
-  //     setSearchYearURL(Number.parseInt(query.get('y')!, 10))
-  //   }
-  // }
-
-  // if searchYear is null THEN SET from response
-  // if (
-  //   response !== null &&
-  //   searchYearLocal === null &&
-  //   response.range !== null &&
-  //   response.range.max_year !== null &&
-  //   searchYearURL !== null
-  // ) {
-  //   console.log('& Setting searchYearLocal from response')
-  //   setSearchYearLocal(response.range.max_year)
-  // }
-
   // This happens when we change search terms and there's no year selected yet
   // OR when we load a page with no year in the URL
-  // searchYearLocal === null &&
   if (
     sliderIsChanging === false &&
     searchYearURL === null &&
     response !== null &&
     response.range !== null &&
-    response.range.max_year !== null &&
-    response.range.max_year != searchYearLocal
+    response.range.min_year !== null &&
+    response.range.min_year !== searchYearLocal
   ) {
-    console.log(
-      '^ No one has a good search year, so set it from the response from',
-      searchYearLocal,
-      'to',
-      response.range.max_year
-    )
-    setSearchYearLocal(response.range.max_year)
+    // console.log(
+    //   '^ No one has a good search year, so set it from the response from',
+    //   searchYearLocal,
+    //   'to',
+    //   response.range.min_year
+    // )
+    setSearchYearLocal(response.range.min_year)
   }
 
-  // if (searchYear !== searchYearLocal) {
-  //   setSearchYearLocal(searchYear)
-  // }
+  const onChange = (_event: React.SyntheticEvent, year: number) => {
+    if (sliderIsChanging === false) {
+      setSliderIsChanging(true)
+    }
 
-  const onChange = (_event: any, value: any) => {
-    // console.log('onChange', value)
-    setSliderIsChanging(true)
-    if (value != searchYearLocal) {
-      console.log('$ Set search year from dragging the slider')
-      setSearchYearLocal(value)
+    if (year !== searchYearLocal) {
+      // console.log('$ Set search year from dragging the slider')
+      setSearchYearLocal(year)
     }
   }
-  // const onChangeDebounced = debounce(onChange, 50, { maxWait: 100 })
 
-  const onChangeCommitted = (_event: any, value: any) => {
+  const onChangeCommitted = (_event: React.SyntheticEvent, year: number) => {
     setSliderIsChanging(false)
-    onDateChange(_event, value)
+    onDateChange(year)
   }
 
   return (
@@ -465,43 +232,19 @@ const TimelineScrubber: React.FC<TimelineScrubberProps> = ({
       {response !== null && response.range !== null && worksPerYear !== null && (
         <Box className={classes.stickToBottom}>
           <Fragment>
-            <ThemeProvider theme={newTheme}>
-              <IOSSlider3
-                min={response.range.min_decade}
-                max={response.range.max_decade}
-                worksPerYear={worksPerYear}
-                // value={searchYearLocal !== null ? response.range.max_decade - searchYearLocal : searchYearLocal}
-                value={searchYearLocal}
-                // defaultValue={response.range.max_year}
-                // defaultValue={searchYearLocal}
-                // defaultValue={query.get('y') !== null ? Number.parseInt(query.get('y')!, 10) : response.range.max_year}
-                // defaultValue={response.searchYear}
-                // defaultValue={searchYear}
-                // defaultValue={searchYear === null ? response.range.max_year : searchYear}
-                // defaultValue={1931}
-                valueLabelFormat={valueLabelFormat}
-                getAriaValueText={valuetext}
-                // aria-labelledby="discrete-slider-restrict"
-                step={null}
-                track={false}
-                valueLabelDisplay="on"
-                marks={getMarks()}
-                onChange={onChange}
-                onChangeCommitted={onChangeCommitted}
-                ValueLabelComponent={StyledValueLabel}
-              />
-            </ThemeProvider>
-
-            <Box className={classes.labelLeft}>
-              <Typography component="span" variant="caption">
-                {response.range.max_decade}
-              </Typography>
-            </Box>
-            <Box className={classes.labelRight}>
-              <Typography component="span" variant="caption">
-                {response.range.min_decade}
-              </Typography>
-            </Box>
+            <SliderSyled
+              min={response.range.min_decade}
+              max={response.range.max_decade}
+              worksPerYear={worksPerYear}
+              value={searchYearLocal}
+              step={null}
+              track={false}
+              valueLabelDisplay="on"
+              marks={getMarks()}
+              onChange={onChange}
+              onChangeCommitted={onChangeCommitted}
+              ValueLabelComponent={StyledValueLabel}
+            />
           </Fragment>
         </Box>
       )}
@@ -509,4 +252,4 @@ const TimelineScrubber: React.FC<TimelineScrubberProps> = ({
   )
 }
 
-export default React.memo(withTheme(TimelineScrubber))
+export default React.memo(TimelineScrubber)
